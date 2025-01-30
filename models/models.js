416dce1ac615122471c1c0525d2f5b87d194cb34
Braima11 <braimamos@gmail.com>
2025-01-30 15:28:51 +0000

    GET /api/articles/:article_id comment_count implemented

--- models/models.js
+++ models/models.js
@@ -19,7 +19,13 @@ exports.getTopicsFromDb = () => {
 
 exports.getArticlesById = (id) => {
 
-    const sqlQuery = `SELECT * FROM articles WHERE article_id =$1`
+    const sqlQuery = `SELECT articles.*,
+               COUNT(comments.comment_id) AS comment_count
+                FROM articles
+                LEFT JOIN comments ON articles.article_id = comments.article_id
+                WHERE articles.article_id=$1
+                GROUP BY articles.article_id
+        `
     return db.query(sqlQuery, [id])
         .then((article) => {
             if (article.rows.length === 0) {
